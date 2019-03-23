5057b3dfee26c72ce432a6e7c2ffbd29f8b81133
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-23 21:56:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,11 +30,12 @@ class Student
     DB[:conn].execute(sql_drop)
   end 
   
-  def save(name, grade)
+  def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
     SQL
+    
     DB[:conn].execute(sql, self.name, self.grade)
   end 
   
