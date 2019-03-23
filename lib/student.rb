ba828ff68e1d244a6143b1112cc7d2b00ddf804c
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-23 21:34:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   
   attr_accessor :id, :name, :grade
   
-  def initialize(name, grade, id: nil)
+  def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
   end 
