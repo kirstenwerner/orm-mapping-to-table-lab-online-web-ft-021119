41dfaf0f7300b6e1e8aac67d8423ba7359c99279
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-23 21:57:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,12 +31,12 @@ class Student
   end 
   
   def save
-    sql = <<-SQL
+    sql_save = <<-SQL
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
       SQL
     
-    DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql_save, self.name, self.grade)
   end 
   
 end
